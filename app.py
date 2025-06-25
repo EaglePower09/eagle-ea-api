1d0e236a9b778680ecf3e513a3cfc69e0d300b3f
EaglePower09 <lonwabod3@gmail.com>
2025-06-25 21:33:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -38,9 +38,9 @@ def send_to_telegram(signal):
         f"Confidence: `{signal['confidence']}`\n"
         f"🕒 Time: `{signal['time']}`"
     )
-    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+    url = f"https://api.telegram.org/bot{7959778482:AAFgqgf01UFX4QCKkYuNBiT4jt557m7LQuE}/sendMessage"
     payload = {
-        "chat_id": TELEGRAM_CHAT_ID,
+        "chat_id": 6105818531,
         "text": text,
         "parse_mode": "Markdown"
     }
